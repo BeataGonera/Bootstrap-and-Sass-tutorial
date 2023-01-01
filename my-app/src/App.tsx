35823a153e25a5d1b564a1fc37dee4629de04f5b
Beata Gonera <beataiwonagonera@gmail.com>
2023-01-01 22:18:28 +0100

    customising Bootstrap

---
+++ my-app/src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <section id="intro">
+
+          <div className="container-lg">
+            <div className="row justify-content-center">
+              <div className="col-md-5 text-center text-md-start">
+                <h1 className="display-2">Black-Belt</h1>
+                <h1 className="display-5 text-muted">your coding skills</h1>
+                <p className="lead my-4 text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+                <a href="#pricing" className="btn btn-mycolour1 btn-lg">Buy now</a>
+              </div>
+            </div>
+
+          </div>
+
+        </section>
+
+     
+    </div>
+  );
+}
+
+export default App;
